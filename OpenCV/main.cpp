43a041e7da496c72a5199a9373472cb7ff3f0d4e
LeeJaeBae <nea4182@gmail.com>
2021-06-07 16:00:50 +0900

    Initial Commit

---
+++ OpenCV/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCV
+//
+//  Created by 이재원 on 2021/06/07.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
